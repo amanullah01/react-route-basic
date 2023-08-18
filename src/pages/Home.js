416299688e40247f66a143e47adc21c471bc576e
Amanullah Aman <amancse.aiub@gmail.com>
2023-08-18 22:34:29 +0600

    Programatically navigate using useNavigate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const HomePage = () => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/product");
+  };
   return (
     <>
       <h1>Hello Home Page</h1>
       <p>
         <Link to="/product">Go to product page</Link>
       </p>
+      <button onClick={navigateHandler}>useNavigate</button>
     </>
   );
 };
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,11 +1,7 @@
-import { Link } from "react-router-dom";
 const ProductPage = () => {
   return (
     <>
       <h1>Product Page</h1>
-      <p>
-        <Link to="/">Go to Home page</Link>
-      </p>
     </>
   );
 };
