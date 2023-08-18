1619e628dc1719f053a2663adb11553fae3ab2af
Amanullah Aman <amancse.aiub@gmail.com>
2023-08-18 21:44:01 +0600

    Adding second route named product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/product",
+    element: <ProductPage />,
   },
 ]);
 
