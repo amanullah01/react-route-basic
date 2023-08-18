0052c629e7592ce15a53bb72bdd1c01a49eb13fc
Amanullah Aman <amancse.aiub@gmail.com>
2023-08-18 23:00:53 +0600

    index route

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "",
+        index: true,
         element: <HomePage />,
       },
       {
