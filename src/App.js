9a47077af821a6ebe48b402d8b3670da683d5789
Amanullah Aman <amancse.aiub@gmail.com>
2023-08-18 21:55:16 +0600

    Link to other page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   createRoutesFromElements,
+//   Route,
+// } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductPage from "./pages/ProductPage";
 
-const routeDefination = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductPage />} />
-  </Route>
-);
+// const routeDefination = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductPage />} />
+//   </Route>
+// );
 
-const router = createBrowserRouter(routeDefination);
+// const router = createBrowserRouter(routeDefination);
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage />,
-//   },
-//   {
-//     path: "/product",
-//     element: <ProductPage />,
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/product",
+    element: <ProductPage />,
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
 const HomePage = () => {
-  return <h1>Hello Home Page</h1>;
+  return (
+    <>
+      <h1>Hello Home Page</h1>
+      <p>
+        <Link to="/product">Go to product page</Link>
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
